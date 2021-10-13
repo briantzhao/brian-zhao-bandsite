d964ebbfd99d7d8a72b267c3758dd51f9e624bef
Brian Zhao <bzhao1991@gmail.com>
2021-10-13 10:49:07 -0400

    Adds JS functionality for creating and validating new comments, adds styling to shows page.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -47,33 +47,40 @@ createList(shows);
 function createList(shows) {
   for (let i = 0; i < shows.length; i++) {
     let show = document.createElement("article");
-    show.classList.add("show__object");
+    show.classList.add("shows__object");
     createChild(show, shows[i], "date");
     createChild(show, shows[i], "venue");
     createChild(show, shows[i], "location");
     let btn = document.createElement("button");
-    btn.classList.add("show__btn");
-    btn.innerText = "Buy Tickets";
+    btn.classList.add("shows__btn");
+    btn.innerText = "BUY TICKETS";
     show.appendChild(btn);
     showsList.appendChild(show);
   }
 }
 
 function createChild(parent, object, key) {
+  let showDiv = document.createElement("div");
+  showDiv.classList.add("shows__item");
+  parent.appendChild(showDiv);
   let showChild = document.createElement("h3");
-  showChild.classList.add("show__label");
+  let showChild1 = document.createElement("h3");
+  showChild1.innerText = object[key];
+  showChild.classList.add("shows__label");
   switch (key) {
     case "date":
       showChild.innerText = "DATE";
+      showChild1.classList.add("shows__date");
       break;
     case "venue":
       showChild.innerText = "VENUE";
+      showChild1.classList.add("shows__venue");
+
       break;
     default:
       showChild.innerText = "LOCATION";
+      showChild1.classList.add("shows__location");
   }
-  parent.appendChild(showChild);
-  let showChild1 = document.createElement("h3");
-  showChild1.innerText = object[key];
-  parent.appendChild(showChild1);
+  showDiv.appendChild(showChild);
+  showDiv.appendChild(showChild1);
 }
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -2,24 +2,27 @@ let comments = [
   {
     name: "Connor Walton",
     date: "02/17/2021",
+    noProfile: true,
     quote:
       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
   {
     name: "Emilie Beach",
     date: "01/09/2021",
+    noProfile: true,
     quote:
       "I feel blessed to have seen them in person. What a comment! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
     name: "Miles Acosta",
     date: "12/20/2020",
+    noProfile: true,
     quote:
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
 
-const commentsList = document.querySelector(".comments__content");
+const commentsList = document.getElementById("comments__content");
 createList(comments);
 function createList(comments) {
   for (let i = 0; i < comments.length; i++) {
@@ -42,8 +45,12 @@ function displayComment(comment) {
   let headSec = document.createElement("div");
   content.classList.add("comments__single");
   content.appendChild(picSec);
+  if (comment.noProfile) {
+    pic.classList.add("comments__profile");
+  } else {
+    pic.classList.add("comments__form__profile");
+  }
   picSec.appendChild(pic);
-  pic.classList.add("comments__profile");
   content.appendChild(textSec);
   textSec.classList.add("comments__text");
   textSec.appendChild(headSec);
@@ -52,4 +59,63 @@ function displayComment(comment) {
   createChild(headSec, comment, "date");
   createChild(textSec, comment, "quote");
   commentsList.appendChild(content);
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (e.target.name.value == "") {
+    formError(e.target.name, true);
+    alert("Please enter a valid name");
+    formError(e.target.comment, false);
+    if (e.target.comment.value == "") {
+      formError(e.target.comment, true);
+      alert("Please enter a valid comment");
+    }
+  } else if (e.target.comment.value == "") {
+    formError(e.target.comment, true);
+    formError(e.target.name, false);
+    alert("Please enter a valid comment");
+  } else {
+    formError(e.target.name, false);
+    formError(e.target.comment, false);
+    let newComment = {};
+    newComment.name = e.target.name.value;
+    newComment.date = todaysDate();
+    newComment.quote = e.target.comment.value;
+    newComment.noProfile = false;
+    comments.unshift(newComment);
+    clearList();
+    createList(comments);
+    let profilePic = document.querySelector(".comments__form__profile");
+    commentsList.firstChild.firstChild.firstChild.style.backgroundImage =
+      profilePic.style.backgroundImage;
+    e.target.name.value = "";
+    e.target.comment.value = "";
+  }
+});
+
+function todaysDate() {
+  const d = new Date();
+  let month;
+  if (d.getMonth() + 1 < 10) {
+    month = "0" + d.getMonth() + 1;
+  } else {
+    month = d.getMonth() + 1;
+  }
+  return month + "/" + d.getDate() + "/" + d.getFullYear();
+}
+
+function clearList() {
+  while (commentsList.lastElementChild) {
+    commentsList.removeChild(commentsList.lastElementChild);
+  }
+}
+
+function formError(field, error) {
+  if (error) {
+    field.style.borderColor = "#D22D2D";
+  } else {
+    field.style.borderColor = "#e1e1e1";
+  }
 }
